7aaca060bd94b07209ca2e5f7478b379abdc2504
VJAY66 <vijaykumaryalla5@gmail.com>
2024-09-27 18:03:25 +0530

    terra commit

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
-    instance_type = "cdt3.micro"
+    instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-06a506a47179173a7"]
 }
